902e46f340e7b66cf667688aa1c3d2b1ea9fa3b6
Steve Kemp <steve@steve.org.uk>
2026-08-19 19:29:46 +0300

    WIP: Add test results, easily
    frodo ~/Repos/github.com/skx/cpm-test $ ./setup.sh
    frodo ~/Repos/github.com/skx/cpm-test $ ./run.sh
    | Test | [cpm](https://github.com/jhallen/cpm) | [cpmulator](https://github.com/skx/cpmulator) | xx | [tnyplo](https://gitlab.com/gbrein/tnylpo)
    | AWRITE.COM |  FAIL | ✔️ | FAIL |
    | BDOSVER.COM |  ✔️ | ✔️ | ✔️ |
    | CONOUT.COM |  ✔️ | ✔️ | ✔️ |
    | CREATE.COM |  ✔️ | ✔️ | ✔️ |
    | CWRITE.COM |  ✔️ | ✔️ | ✔️ |
    | DELETE.COM |  ✔️ | ✔️ | ✔️ |
    | DRIVE.COM |  FAIL | ✔️ | FAIL |
    | FILESIZE.COM |  FAIL | ✔️ | ✔️ |
    | IOPORT.COM |  ✔️ | ✔️ | ✔️ |
    | RANDOMRW.COM |  ✔️ | ✔️ | ✔️ |
    | RANDREC.COM |  ✔️ | ✔️ | ✔️ |
    | READWRITE.COM |  FAIL | ✔️ | FAIL |
    | RENAME.COM |  ✔️ | ✔️ | ✔️ |
    | SEARCH.COM |  ✔️ | ✔️ | ✔️ |
    | SETDMA.COM |  ✔️ | ✔️ | ✔️ |
    | USERNUM.COM |  FAIL | ✔️ | ✔️ |
    | WSTRING.COM |  ✔️ | ✔️ | ✔️ |
    
    Looks like this is a decent framework.

---
+++ run.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# For each of our tests run them with a list of
+# known emulators
+#
+
+
+
+#
+# Print a header
+#
+echo "| Test | [cpm](https://github.com/jhallen/cpm) | [cpmulator](https://github.com/skx/cpmulator) | xx | [tnyplo](https://gitlab.com/gbrein/tnylpo)"
+
+
+#
+# Did running the given command produce "PASSED" in stdout?
+#
+# Hide STDERR.
+#
+function wasPass {
+    "$@" > out.tmp 2>&1
+
+    if ( grep --silent "PASSED" out.tmp ) ; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+
+#
+# For each test
+#
+for file in *.COM; do
+
+
+    #
+    # Show the name of the test
+    #
+    printf "| %s | " "${file}"
+
+
+    #
+    # CPM - copy to a lower-cased name, and execute without the ".com" suffix
+    #
+    cp "${file}" "tmp.com"
+    cmd=(./cpm/cpm --exec tmp)
+    if wasPass "${cmd[@]}" ; then
+        printf " ✔️ |"
+    else
+        printf " FAIL |"
+    fi
+    rm "tmp.com"
+
+    #
+    # CPMULATOR - keep the name as-is
+    #
+    cmd=(./cpmulator/cpmulator --input=stty "${file}")
+    if wasPass "${cmd[@]}" ; then
+        printf " ✔️ |"
+    else
+        printf " FAIL |"
+    fi
+
+    #
+    # iz-cpm
+    #
+
+    #
+    # tnylpo
+    #
+    # This wants the filename in lower-case - and without the .com suffix
+    #
+    cp "${file}" "tmp.com"
+    cmd=(./tnylpo/tnylpo tmp)
+    if wasPass "${cmd[@]}" ; then
+        printf " ✔️ |"
+    else
+        printf " FAIL |"
+    fi
+    rm "tmp.com"
+
+    #
+    # End of line
+    #
+    echo ""
+
+done
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Clone each of the emulators we run our tests with.
+#
+
+#
+# The list of source URLs we clone
+#
+
+if [ ! -d cpm ] ; then
+    git clone https://github.com/jhallen/cpm.git
+    ( cd cpm && make )
+fi
+
+if [ ! -d cpmulator ] ; then
+    git clone https://github.com/skx/cpmulator.git
+    ( cd cpmulator && go build . )
+fi
+
+if [ ! -d tnylpo ] ; then
+    git clone https://gitlab.com/gbrein/tnylpo.git
+    ( cd tnylpo && make )
+fi
