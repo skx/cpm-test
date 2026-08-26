43bc0e6739e3d1b197d87c3e3519167f81a71315
Steve Kemp <steve@steve.org.uk>
2026-08-26 19:03:43 +0300

    If the repositories are cloned update them, to ensure we're always testing the most recent revision of each given emulator.

--- setup.sh
+++ setup.sh
@@ -1,33 +1,57 @@
 #!/bin/sh
 #
-# Clone each of the emulators we run our tests with.
+# Checkout each of the emulators we run our tests with, and
+# update from the local repositories as necessary.
 #
 
-#
-# The list of source URLs we clone
-#
 
+
+# cpm
 if [ ! -d cpm ] ; then
     git clone https://github.com/jhallen/cpm.git
-    ( cd cpm && make )
+else
+    (cd cpm && git pull)
 fi
+( cd cpm && make )
 
+
+
+# cpmulator
 if [ ! -d cpmulator ] ; then
     git clone https://github.com/skx/cpmulator.git
-    ( cd cpmulator && go build . )
+else
+    (cd cpmulator && git pull)
 fi
+( cd cpmulator && go build . )
 
-if [ ! -d ntvcm ]; then
-    git clone https://github.com/davidly/ntvcm.git
-    ( cd ntvcm && make )
-fi
 
-if [ ! -d tnylpo ] ; then
-    git clone https://gitlab.com/gbrein/tnylpo.git
-    ( cd tnylpo && make )
-fi
 
+# iz-cpm
 if [ ! -d iz-cpm ] ; then
     git clone https://github.com/ivanizag/iz-cpm.git
-    ( cd iz-cpm && cargo build )
+else
+    (cd iz-cpm && git pull)
 fi
+( cd iz-cpm && cargo build )
+
+
+
+# ntvcm
+if [ ! -d ntvcm ]; then
+    git clone https://github.com/davidly/ntvcm.git
+else
+    (cd ntvcm && git pull)
+fi
+( cd ntvcm && make )
+
+
+
+# tnylpo
+if [ ! -d tnylpo ] ; then
+    git clone https://gitlab.com/gbrein/tnylpo.git
+else
+    (cd tnylpo && git pull)
+fi
+( cd tnylpo && make )
+
+
